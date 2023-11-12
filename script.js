3b28f18e99de706b7915bba558ed98cb50d26ce3
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-11-12 20:19:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,6 +31,17 @@ const questions = [
 ];
 
 // Display the quiz questions and choices
+let userAnswers = [];
+
+// Retrieve saved progress from session storage
+const savedProgress = sessionStorage.getItem("progress");
+if (savedProgress) {
+  userAnswers = JSON.parse(savedProgress);
+}
+
+// Display the quiz questions and choices
+const questionsElement = document.getElementById("questions");
+
 function renderQuestions() {
   for (let i = 0; i < questions.length; i++) {
     const question = questions[i];
@@ -43,6 +54,12 @@ function renderQuestions() {
       choiceElement.setAttribute("type", "radio");
       choiceElement.setAttribute("name", `question-${i}`);
       choiceElement.setAttribute("value", choice);
+      choiceElement.addEventListener("change", function () {
+        // Update userAnswers array when the user selects an option
+        userAnswers[i] = choice;
+        // Save progress in session storage
+        sessionStorage.setItem("progress", JSON.stringify(userAnswers));
+      });
       if (userAnswers[i] === choice) {
         choiceElement.setAttribute("checked", true);
       }
@@ -53,4 +70,25 @@ function renderQuestions() {
     questionsElement.appendChild(questionElement);
   }
 }
+
+// Render questions on page load
 renderQuestions();
+
+// Submit button event listener
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", function () {
+  // Calculate the score
+  let score = 0;
+  for (let i = 0; i < questions.length; i++) {
+    if (userAnswers[i] === questions[i].answer) {
+      score++;
+    }
+  }
+
+  // Display the score on the page
+  const scoreElement = document.getElementById("score");
+  scoreElement.textContent = `Your score is ${score} out of 5.`;
+
+  // Store the score in local storage
+  localStorage.setItem("score", score);
+});
